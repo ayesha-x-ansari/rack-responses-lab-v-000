de3ea929fd9250c6089a752ae419961e0446db75
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-20 22:04:18 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,10 +2,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = Time.new
+  #  time = Time.new
     #set 'time' equal to the current time.
 
-    time = time.hour.to_s
+  #  time = time.hour.to_s
 
     if time  <= "12"
       resp.write "Good Morning"
